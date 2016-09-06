7c8f77d08db5e9b414f91f0f19422c263c746a61
Ostrea <Ostrea@users.noreply.github.com>
2016-09-06 23:44:26 +0300

    Add title to the help page

--- twitter_clone_app/tests.py
+++ twitter_clone_app/tests.py
@@ -17,12 +17,14 @@ class ViewTests(TestCase):
 
     def test_help_view(self):
         """
-        Should get 'help.html' template.
+        Should get 'help.html' template with appropriate title.
         """
         response = self.client.get(reverse('twitter_clone_app:help'))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'twitter_clone_app/help.html')
+        self.assertContains(response, '<title>Help | Fake Twitter</title>',
+                            html=True)
 
     def test_about_view(self):
         """
