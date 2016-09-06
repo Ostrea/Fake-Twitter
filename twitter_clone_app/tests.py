8755ed16a78f4aaeee0217ce340c3fb39c2a83d2
Ostrea <Ostrea@users.noreply.github.com>
2016-09-06 23:08:57 +0300

    Add tests for index page

--- twitter_clone_app/tests.py
+++ twitter_clone_app/tests.py
@@ -1,3 +1,15 @@
+from django.urls import reverse
 from django.test import TestCase
 
-# Create your tests here.
+
+class ViewTests(TestCase):
+
+    def test_index_view(self):
+        """
+        Should get 'index.html' template.
+        """
+        response = self.client.get(reverse('twitter_clone_app:index'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name,
+                         "twitter_clone_app/index.html")
