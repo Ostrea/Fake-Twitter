3f0afad4c7d976bc038f085862a372662cfd14e7
Ostrea <Ostrea@users.noreply.github.com>
2016-09-13 15:27:59 +0300

    Add root for create user

--- twitter_clone_app/urls.py
+++ twitter_clone_app/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^about/$', views.about, name='about'),
     url(r'^contact/$', views.contact, name='contact'),
     url(r'^signup$', views.sign_up, name='sign-up'),
+    url(r'^create-user$', views.create_user, name='create-user'),
 
     # /users/<user_id>/
     url(r'^users/(?P<user_id>[0-9]+)/$', views.user_profile,
