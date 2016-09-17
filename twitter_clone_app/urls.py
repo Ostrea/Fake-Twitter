25a558029d53bd625c262b521e9c7d2766879641
Ostrea <Ostrea@users.noreply.github.com>
2016-09-17 18:32:43 +0300

    Add logout route.

--- twitter_clone_app/urls.py
+++ twitter_clone_app/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     url(r'^signup$', views.sign_up, name='sign-up'),
     url(r'^create-user$', views.create_user, name='create-user'),
     url(r'^login$', views.log_in, name='log-in'),
+    url(r'^logout$', views.log_out, name='log-out'),
 
     # /users/<user_id>/
     url(r'^users/(?P<user_id>[0-9]+)/$', views.user_profile,
