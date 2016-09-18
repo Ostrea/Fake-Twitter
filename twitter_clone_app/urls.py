f5dafe18be839aaf6d2132da94a1f33a71b47c6f
Ostrea <Ostrea@users.noreply.github.com>
2016-09-18 12:36:52 +0300

    Add ending slash to the urls.

--- twitter_clone_app/urls.py
+++ twitter_clone_app/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     url(r'^help/$', views.help, name='help'),
     url(r'^about/$', views.about, name='about'),
     url(r'^contact/$', views.contact, name='contact'),
-    url(r'^signup$', views.sign_up, name='sign-up'),
-    url(r'^create-user$', views.create_user, name='create-user'),
-    url(r'^login$', views.log_in, name='log-in'),
-    url(r'^logout$', views.log_out, name='log-out'),
+    url(r'^signup/$', views.sign_up, name='sign-up'),
+    url(r'^create-user/$', views.create_user, name='create-user'),
+    url(r'^login/$', views.log_in, name='log-in'),
+    url(r'^logout/$', views.log_out, name='log-out'),
 
     # /users/<user_id>/
     url(r'^users/(?P<user_id>[0-9]+)/$', views.user_profile,
