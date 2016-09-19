8f37c1efd8a59cf06ba372ff0e25700eff6e76be
Ostrea <Ostrea@users.noreply.github.com>
2016-09-19 12:42:33 +0300

    Add show all users route.

--- twitter_clone_app/urls.py
+++ twitter_clone_app/urls.py
@@ -17,9 +17,11 @@ urlpatterns = [
     url(r'^login/$', views.log_in, name='log-in'),
     url(r'^logout/$', views.log_out, name='log-out'),
 
+    # Users urls.
     # /users/<user_id>/
     url(r'^users/(?P<user_id>[0-9]+)/$', views.user_profile,
         name='user-profile'),
+    url(r'^users/all/$', views.show_all_users, name='show-all-users'),
 
-    url(r'^edit/$', views.edit_user, name='edit-user'),
+    url(r'^edit/$', views.edit_user, name='edit-user')
 ]
