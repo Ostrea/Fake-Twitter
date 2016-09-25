c000713223350b2cd250cef0feb6d5a04e53a340
Ostrea <Ostrea@users.noreply.github.com>
2016-09-25 13:46:45 +0300

    Add micropost model.

--- twitter_clone_app/models.py
+++ twitter_clone_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+import django.contrib.auth.models as auth_models
+
+
+class Micropost(models.Model):
+    content = models.TextField(max_length=140)
+    user = models.ForeignKey(auth_models.User)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
